277399800738cae2cfcc8d0a557fc2791053e8fe
Damijan Kozic <damijan.poglejsi@gmail.com>
2023-09-05 01:58:05 +0200

    fix: do not call a function in expect()

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ use zero2prod::run;
 async fn main() -> Result<(), std::io::Error> {
     let ip = "127.0.0.1";
     let port = "8000";
-    let address = format!("{}:{}", ip, port);
 
-    let listener = TcpListener::bind(&address).expect(&format!("Failed to bind {}", &address));
+    let address = format!("{}:{}", ip, port);
+    let expect_message = format!("Failed to bind {}", &address);
+
+    let listener = TcpListener::bind(&address).expect(&expect_message);
 
     run(listener)?.await
 }
