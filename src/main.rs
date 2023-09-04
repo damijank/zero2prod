9701101a7df92432a251d60bea48f37b7c47bb28
Damijan Kozic <damijan.poglejsi@gmail.com>
2023-09-05 01:50:32 +0200

    add health check; add integration test for health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let ip = "127.0.0.1";
+    let port = "8000";
+    let address = format!("{}:{}", ip, port);
+
+    let listener = TcpListener::bind(&address).expect(&format!("Failed to bind {}", &address));
+
+    run(listener)?.await
 }
