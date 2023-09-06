74be5a0cb28efb9cbb9df565119b949e8d4d8d7f
Damijan Kozic <damijan.poglejsi@gmail.com>
2023-09-06 12:10:18 +0200

    write subscription to database; isolated DBs for tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -6,11 +7,14 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     let ip = "127.0.0.1";
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("{}:{}", ip, configuration.application_port);
     let expect_message = format!("Failed to bind {}", &address);
 
     let listener = TcpListener::bind(&address).expect(&expect_message);
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
