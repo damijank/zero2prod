e7460a423956dffabbdb50f39e6b913e4be16f53
Damijan Kozic <damijan.poglejsi@gmail.com>
2023-10-02 03:02:12 +0200

    refactor application startup to be reusable for tests

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,10 +1,8 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -26,50 +24,33 @@ pub struct TestApp {
 
 // Launch our application in the background
 pub async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
     Lazy::force(&TRACING);
 
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("127.0.0.1:{}", port);
+    let configuration = {
+        let mut configuration = get_configuration().expect("Failed to read configuration");
+        configuration.database.database_name = Uuid::new_v4().to_string();
+        configuration.application.port = 0;
+        configuration
+    };
 
-    let mut configuration = get_configuration().expect("Failed to read configuration");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_db(&configuration.database).await;
+    configure_db(&configuration.database).await;
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+    let application = Application::build(configuration.clone())
+        .await
+        .expect("Failed to build application.");
+    let address = format!("127.0.0.1:{}", application.port());
 
-    let server =
-        run(listener, connection_pool.clone(), email_client).expect("Failed to bind address");
-
-    // for line
-    // let _ = tokio::spawn(server);
-    //
-    // `cargo clippy --fix` suggests to
-    //   - await the result since the spawn function is async... we can not and do not want to do that here,
-    //   - or, drop the result with `std::mem::drop` like
-    // std::mem::drop(tokio::spawn(server));
-    //
-    // not assigning the result at all (probably?) has the same effect
-    tokio::spawn(server);
+    tokio::spawn(application.run_until_stopped());
 
     TestApp {
         address,
-        connection_pool,
+        connection_pool: get_connection_pool(&configuration.database),
     }
 }
 
-pub async fn configure_db(config: &DatabaseSettings) -> PgPool {
+pub async fn configure_db(config: &DatabaseSettings) {
     let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres.");
@@ -85,6 +66,4 @@ pub async fn configure_db(config: &DatabaseSettings) -> PgPool {
         .run(&connection_pool)
         .await
         .expect("Failed to migrate the database");
-
-    connection_pool
 }
